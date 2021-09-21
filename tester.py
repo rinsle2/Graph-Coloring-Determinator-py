80e0c2cea3c21bf98facbf051b1aee4019433e84
rinsle2 <32503348+rinsle2@users.noreply.github.com>
2021-09-21 08:22:20 -0500

    Updated Tester

--- tester.py
+++ tester.py
@@ -1,45 +1,48 @@
 import unittest
-import Graph as g
+
+import Graph
 
 
-def petersonGraph(n: g):
-    n.addEdge(1, 2)
-    n.addEdge(1, 5)
-    n.addEdge(1, 6)
-    n.addEdge(2, 7)
-    n.addEdge(2, 3)
-    n.addEdge(3, 8)
-    n.addEdge(3, 4)
-    n.addEdge(4, 9)
-    n.addEdge(4, 5)
-    n.addEdge(5, 10)
-    n.addEdge(6, 8)
-    n.addEdge(6, 9)
-    n.addEdge(8, 10)
-    n.addEdge(10, 7)
-    n.addEdge(7, 9)
+def square_graph(n):
+    n.add_edge(1, 2)
+    n.add_edge(2, 3)
+    n.add_edge(3, 4)
+    n.add_edge(4, 1)
+    n.add_edge(4, 2)
+    n.add_edge(1, 3)
 
 
-def squareGraph(n: g):
-    n.addEdge(1, 2)
-    n.addEdge(2, 3)
-    n.addEdge(3, 4)
-    n.addEdge(4, 1)
-    n.addEdge(4, 2)
-    n.addEdge(1, 3)
+def peterson_graph(n):
+    n.add_edge(1, 2)
+    n.add_edge(1, 5)
+    n.add_edge(1, 6)
+    n.add_edge(2, 7)
+    n.add_edge(2, 3)
+    n.add_edge(3, 8)
+    n.add_edge(3, 4)
+    n.add_edge(4, 9)
+    n.add_edge(4, 5)
+    n.add_edge(5, 10)
+    n.add_edge(6, 8)
+    n.add_edge(6, 9)
+    n.add_edge(8, 10)
+    n.add_edge(10, 7)
+    n.add_edge(7, 9)
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_square(self):
-        n = g.Graph()
-        squareGraph(n)
-        self.assertEqual(n.isColorable(3), False)  # add assertion here
+        n = Graph.make_graph()
+        square_graph(n)
+        self.assertEqual(n.is_colorable(3), False)  # add assertion here
+        del n
 
     def test_peterson(self):
-        n = g.Graph()
-        petersonGraph(n)
-        self.assertEqual(n.isColorable(3), True)
+        n = Graph.make_graph()
+        peterson_graph(n)
+        self.assertEqual(n.is_colorable(3), True)
+        del n
 
 
 if __name__ == '__main__':
